d3f502801a12b0bb574eb0af052346d880730432
ayuprac <ayushkhorgade2002@gmail.com>
2025-04-08 12:04:30 +0530

    nested if else

--- one.py
+++ one.py
@@ -6,3 +6,14 @@ elif num < 0:
     print('Negative')
 else:
     print('Zero')
+    #_____________________________________________
+
+#nested if else
+num = 15
+if num>=0:
+    if num==0:
+        print('Zero')
+    else:
+        print("Positive")
+else:
+    print("Negative")
